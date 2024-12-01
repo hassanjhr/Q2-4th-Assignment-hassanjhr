c57df86007e8a1078fe2cdb208692e344dfde8af
Muhammad Hassan Jawaid <141501919+hassanjhr@users.noreply.github.com>
2024-12-01 22:14:01 +0500

    commit-five

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <>
       {/* section 1 */}
-      <div className="  sm:mx-[100px] md:mx-[100px] mx-auto     lg:flex   w-[358px] h-[300px] top-[3490px] left-[16px] rounded-[20px] sm:w-[358px] sm:h-[300px]   sm:top-[4550px] sm:left-[16px]  sm:rounded-[20px] md:flex sm:justify-between    md:w-[550px] md:h-[180px] md:top-[4575px]  md:left-[2px] md:rounded-[20px] md:justify-between md:py-[36px] md:px-[25px]   xl:flex xl:w-[1120px] 2xl:w-[1240px] xl:h-[180px] xl:top-[3881px] xl:left-[20px] xl:rounded-[20px] xl:justify-between xl:py-[36px] xl:px-[64px]  bg-black absolute   lg:w-[800px] lg:h-[180px] lg:top-[3950px] lg:left-[2px] lg:rounded-[20px] lg:justify-between lg:py-[36px] lg:px-[64px]">
+      <div className="  sm:mx-[100px] md:mx-[100px] mx-auto     lg:flex   w-[358px] h-[300px] top-[3490px] left-[16px] rounded-[20px] sm:w-[358px] sm:h-[300px]   sm:top-[4550px] sm:left-[16px]  sm:rounded-[20px] md:flex sm:justify-between    md:w-[550px] md:h-[180px] md:top-[4575px]  md:left-[2px] md:rounded-[20px] md:justify-between md:py-[36px] md:px-[25px]   xl:flex xl:w-[1240px] 2xl:w-[1240px] xl:h-[180px] xl:top-[3881px] xl:left-[20px] xl:rounded-[20px] xl:justify-between xl:py-[36px] xl:px-[64px]  bg-black absolute   lg:w-[800px] lg:h-[180px] lg:top-[3950px] lg:left-[2px] lg:rounded-[20px] lg:justify-between lg:py-[36px] lg:px-[64px]">
         {/* 1 */}
         <div className="lg:w-[600px] lg:h-[94px] md:w-[600px] md:h-[94px] sm:w-[358px] sm:h-[105px]  ">
           <h2 className="pt-[10px] md:pt-0   text-white font-bold md:text-[16px] lg:text-[25px] xl:text-[40px] md:leading-[45px] sm:text-[30px] sm:px-[45px] sm:top-[32px] sm:left-[24px] text-[30px] px-[45px] top-[32px] left-[24px]">
@@ -61,7 +61,7 @@ const Footer = () => {
       {/* Section 2 */}
       <div className=" lg:h-[499px] md:h-[650px]  h-[846px] w-auto mt-[170px] md:mt-0  bg-[#F0F0F0] ">
         {/* footer */}
-        <div className="xl:w-[1240px] 2xl:w-[1340px] lg:w-[900px] md:w-[650px] md:h-[177px] md:flex md:justify-between md:gap-1 lg:gap-0  md:pl-[110px] pt-[180px] pl-[] md:pt-[120px] lg:pt-[140px] ">
+        <div className="xl:w-[1300px] 2xl:w-[1340px] lg:w-[900px] md:w-[650px] md:h-[177px] md:flex md:justify-between md:gap-1 lg:gap-0  md:pl-[110px] pt-[180px] pl-[] md:pt-[120px] lg:pt-[140px] xl:pl-[120px] ">
           {/* 1 */}
           <div className="md:w-[180px] lg:w-[248px] md:h-[210px] lg:h-[177px] pl-[20px] md:pl-0">
             <div className="md:w-[140px] lg:w-[167px] md:h-[23px]">
@@ -82,7 +82,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="grid lg:grid-cols-4 grid-cols-2 pl-[20px] md:pl-0 ">
+          <div className="grid lg:grid-cols-4 grid-cols-2 pl-[20px] md:pl-0 xl:gap-8">
             {/* 2 */}
             <div className="md:w-[104px] md:h-[210px] lg:h-[177px]  ">
               <h2 className="font-medium text-[14px] lg:text-[16px] leading-[18px] tracking-[0.3em] md:mb-[15px] ">
